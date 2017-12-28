dbb800ae5dab42a29bdd41e3b53fb63c2e618a72
祝令昊 <444920039@qq.com>
2017-12-28 19:03:54 +0800

    end end

--- 2017something/js/app.js
+++ 2017something/js/app.js
@@ -1,9 +1,3 @@
-function bbimg(o){ 
-	var zoom=parseInt(o.style.zoom, 10)||100;
-	zoom+=event.wheelDelta/12;
-	if (zoom>0) o.style.zoom=zoom+'%';
-	return false;
-}
 window.onload = function() {
 	//走马灯开始
 	var zmdspeed=2;
